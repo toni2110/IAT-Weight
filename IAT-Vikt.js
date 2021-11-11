7847d82166d2e4d839574d6e870e354ad727c6e1
toni2110 <92384247+toni2110@users.noreply.github.com>
2021-11-11 13:07:47 +0100

    Update IAT-Vikt.js

--- IAT-Vikt.js
+++ IAT-Vikt.js
@@ -3,43 +3,48 @@ define(['pipAPI', 'https://toni2110.github.io/IAT-Weight/IAT-Vikt-quiat10.js'], 
 
 	return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
+			name : 'överviktig', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {word : 'Överviktig'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		   	{image : '1_F_COLOR.jpg'}, 
+    			{image : '2_F_COLOR.jpg'}, 
+    			{image : '3_F_COLOR.jpg'}, 
+			{image : '4_F_COLOR.jpg'}, 
+    			{image : '5_F_COLOR.jpg'},
+			{image : 'W_1_F.jpg'},
+			{image : 'w_2_F.jpg'},
+			{image : 'W_3_F.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'White people', //Will appear in the data.
+			name : 'normalviktig', //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
+				media : {word : 'Normalviktig'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
+    			{image : '1_S_COLOR.jpg'}, 
+    			{image : '2_S_COLOR.jpg'}, 
+    			{image : '3_S_COLOR.jpg'}, 
+    			{image : '4_S_COLOR.jpg'}, 
+			{image : '5_S_COLOR.jpg'}, 
+			{image : 'W_1_s.JPG'}, 
+			{image : 'w_2_S.jpg'}, 
+    			{image : 'W_3_S.jpg'}			
+			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/'
+			image : 'https://toni2110.github.io/IAT-Weight/IAT-bilderna'
 		} 
 	});
 });
